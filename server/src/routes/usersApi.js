da4779e718f1588bd59f164f567f460804b88840
mma-tahmid <mma.tahmid@gmail.com>
2024-05-22 21:57:33 +0600

    setup react & express js

---
+++ server/src/routes/usersApi.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+// const userControllers = require("../controllers/UserControllers");
+// const { CheckNormalUser, CheckAdmin } = require("../middlewares/authMiddleware");
+
+
+
+const router = express.Router();
+
+
+// router.post("/registration", userControllers.Registration);
+// router.post("/login", userControllers.Login);
+
+
+
+
+
+
+module.exports = router;
